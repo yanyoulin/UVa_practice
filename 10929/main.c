9384d7a1ab3285d464732526ad966b62b1143a10
yanyoulin <yoyo084669@gmail.com>
2024-07-20 12:16:02 +0800

    Update main.c

--- 10929/main.c
+++ 10929/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/*
+    因為輸入的數字太大了，我們要用字串處理
+*/
 int main()
 {
     char d[1001];
@@ -12,7 +14,7 @@ int main()
         for(int i = 0; i<strlen(d); i++)
         {
             if(i%2 == 1)
-                sum_odd += d[i] - '0';
+                sum_odd += d[i] - '0'; //字元減掉'0'才會是數字
             else
                 sum_even += d[i] - '0';
         }
